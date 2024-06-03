d01f57daffc4fbb72029eb85cb41575ef234b421
Akt99 <157685968+Akt99@users.noreply.github.com>
2024-06-03 19:25:57 +0530

    Update main (1).py
    High score feature added

--- main (1).py
+++ main (1).py
@@ -9,7 +9,7 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("The Vintage Snake Game ")
 screen.tracer(0)
-#starting_positions=[(0,0),(-20,0),(-40,0)]
+
 scoreboard=Scoreboard()
 snake=Snake()
 food=Food()
@@ -32,14 +32,14 @@ while game_is_on:
          #print("eat")for testing
  #detect collision with wall        
     if snake.head.xcor()>290 or snake.head.xcor()<-290 or snake.head.ycor()<-290 or snake.head.ycor()>290:
-        game_is_on=False
-        scoreboard.game_over()
+        snake.reset()
+        scoreboard.reset()
 
 #detect collision with own body and the head
     for segment in snake.segments[1:]:
         if snake.head.distance(segment)<10:
             game_is_on=False
-            scoreboard.game_over()
+            scoreboard.reset()
 
         
 
