8e23204b1b6952c7dd274a1ef843d08f9134adfd
Arnab <157685968+Akt99@users.noreply.github.com>
2025-05-04 11:37:23 +0530

    Update food.py
    Changed color of food particle

--- food.py
+++ food.py
@@ -6,7 +6,7 @@ class Food(Turtle):
         self.shape("circle")
         self.penup()
         self.shapesize(stretch_len=0.5, stretch_wid=0.5)
-        self.color("blue")
+        self.color("#9F002A")
         self.speed("fastest")
         self.refresh()  
 
